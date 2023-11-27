d3b593c461aea8210c60a28fccee6fd19d61ad8c
Will Hooper <whooper@nygenome.org>
2023-11-27 15:54:27 -0500

    Add stubs for custom analysis code

---
+++ analysis/03-signatures/run-03-signatures.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+################################################################################
+### COPYRIGHT ##################################################################
+
+# New York Genome Center
+
+# SOFTWARE COPYRIGHT NOTICE AGREEMENT
+# This software and its documentation are copyright (2023) by the New York
+# Genome Center. All rights are reserved. This software is supplied without
+# any warranty or guaranteed support whatsoever. The New York Genome Center
+# cannot be responsible for its use, misuse, or functionality.
+
+# Author: William F. Hooper & Timothy R. Chu 
+
+################################################################# /COPYRIGHT ###
+################################################################################
+
